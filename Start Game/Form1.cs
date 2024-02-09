056a6998c67862fe2c898529234052d10c91741d
GarrettAckersviller <garracke201@ed.amdsb.ca>
2024-02-09 14:08:29 -0500

    Add project files.

---
+++ Start Game/Form1.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Start_Game
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label2.BackColor = Color.White;
+            label3.BackColor = Color.White;
+            label4.BackColor = Color.White;
+            label6.BackColor = Color.White;
+
+            Refresh();
+            Thread.Sleep(1000);
+
+            label2.BackColor = Color.Blue;
+            label3.BackColor = Color.White;
+            label4.BackColor = Color.White;
+            label6.BackColor = Color.White;
+
+            Refresh();
+            Thread.Sleep(100);
+
+            label2.BackColor = Color.White;
+            label3.BackColor = Color.Blue;
+            label4.BackColor = Color.White;
+            label6.BackColor = Color.White;
+
+            Refresh();
+            Thread.Sleep(100);
+
+            label2.BackColor = Color.White;
+            label3.BackColor = Color.White;
+            label4.BackColor = Color.Blue;
+            label6.BackColor = Color.White;
+
+            Refresh();
+            Thread.Sleep(100);
+
+            label2.BackColor = Color.White;
+            label3.BackColor = Color.White;
+            label4.BackColor = Color.White;
+            label6.BackColor = Color.Blue;
+
+            Refresh();
+            Thread.Sleep(100);
+
+            label2.BackColor = Color.Blue;
+            label3.BackColor = Color.White;
+            label4.BackColor = Color.White;
+            label6.BackColor = Color.White;
+
+            Refresh();
+            Thread.Sleep(100);
+
+            label2.BackColor = Color.White;
+            label3.BackColor = Color.Blue;
+            label4.BackColor = Color.White;
+            label6.BackColor = Color.White;
+
+            Refresh();
+            Thread.Sleep(100);
+
+            label2.BackColor = Color.White;
+            label3.BackColor = Color.White;
+            label4.BackColor = Color.Blue;
+            label6.BackColor = Color.White;
+
+            Refresh();
+            Thread.Sleep(100);
+
+            label2.BackColor = Color.White;
+            label3.BackColor = Color.White;
+            label4.BackColor = Color.White;
+            label6.BackColor = Color.Blue;
+
+            Refresh();
+            Thread.Sleep(100);
+
+            label2.BackColor = Color.Blue;
+            label3.BackColor = Color.White;
+            label4.BackColor = Color.White;
+            label6.BackColor = Color.White;
+
+            Refresh();
+            Thread.Sleep(100);
+
+            label2.BackColor = Color.White;
+            label3.BackColor = Color.White;
+            label4.BackColor = Color.White;
+            label6.BackColor = Color.White;
+
+            Refresh();
+            Thread.Sleep(1000);
+
+            label2.BackColor = Color.Blue;
+            label3.BackColor = Color.White;
+            label4.BackColor = Color.White;
+            label6.BackColor = Color.White;
+
+            Refresh();
+            Thread.Sleep(100);
+
+            label2.BackColor = Color.White;
+            label3.BackColor = Color.Blue;
+            label4.BackColor = Color.White;
+            label6.BackColor = Color.White;
+
+            Refresh();
+            Thread.Sleep(100);
+
+            label2.BackColor = Color.White;
+            label3.BackColor = Color.White;
+            label4.BackColor = Color.Blue;
+            label6.BackColor = Color.White;
+
+            Refresh();
+            Thread.Sleep(100);
+
+            label2.BackColor = Color.White;
+            label3.BackColor = Color.White;
+            label4.BackColor = Color.White;
+            label6.BackColor = Color.Blue;
+
+            Refresh();
+            Thread.Sleep(100);
+
+            label2.BackColor = Color.Blue;
+            label3.BackColor = Color.White;
+            label4.BackColor = Color.White;
+            label6.BackColor = Color.White;
+
+            Refresh();
+            Thread.Sleep(100);
+
+            label2.BackColor = Color.White;
+            label3.BackColor = Color.Blue;
+            label4.BackColor = Color.White;
+            label6.BackColor = Color.White;
+
+            Refresh();
+            Thread.Sleep(100);
+
+            label2.BackColor = Color.White;
+            label3.BackColor = Color.White;
+            label4.BackColor = Color.Blue;
+            label6.BackColor = Color.White;
+
+            Refresh();
+            Thread.Sleep(100);
+
+            label2.BackColor = Color.White;
+            label3.BackColor = Color.White;
+            label4.BackColor = Color.White;
+            label6.BackColor = Color.Blue;
+
+            Refresh();
+            Thread.Sleep(100);
+
+            label2.BackColor = Color.Blue;
+            label3.BackColor = Color.White;
+            label4.BackColor = Color.White;
+            label6.BackColor = Color.White;
+
+            Refresh();
+            Thread.Sleep(100);
+
+            this.BackColor = Color.Yellow;
+            label2.BackColor = Color.Yellow;
+            label3.BackColor = Color.Yellow;
+            label4.BackColor = Color.Yellow;
+            label6.BackColor = Color.Yellow;
+            label1.ForeColor = Color.Yellow;
+            button1.BackColor = Color.Yellow;
+       
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
